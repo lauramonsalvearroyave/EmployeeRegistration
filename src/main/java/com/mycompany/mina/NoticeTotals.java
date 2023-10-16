d7578fd21234ba92f0c07548271e8d45df61c174
Laura Monsalve <lmonsalve2219@gmail.com>
2023-10-15 22:06:05 -0500

    Refactor Employee Registration WorkShop 3

---
+++ src/main/java/com/mycompany/mina/NoticeTotals.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mina;
+
+/**
+ *
+ * @author Laura Monsalve
+ */
+public class NoticeTotals {
+
+    public int getTotalTemporalDays() {
+        return totalTemporalDays;
+    }
+
+    public void setTotalTemporalDays(int totalTemporalDays) {
+        this.totalTemporalDays = totalTemporalDays;
+    }
+
+    public int getTotalMotherhoodDays() {
+        return totalMotherhoodDays;
+    }
+
+    public void setTotalMotherhoodDays(int totalMotherhoodDays) {
+        this.totalMotherhoodDays = totalMotherhoodDays;
+    }
+
+    public int getTotalFatherhoodDays() {
+        return totalFatherhoodDays;
+    }
+
+    public void setTotalFatherhoodDays(int totalFatherhoodDays) {
+        this.totalFatherhoodDays = totalFatherhoodDays;
+    }
+    private int totalTemporalDays;
+    private int totalMotherhoodDays;
+    private int totalFatherhoodDays;
+}
